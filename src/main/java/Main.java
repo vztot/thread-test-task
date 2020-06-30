ca511fe9b4467c91cc40ccbc7fb01cc4b582e32e
Oleksandr Husenko <oleksandrhusenko@gmail.com>
2020-06-30 13:18:39 +0300

    Added "Thread race"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,18 @@
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
 public class Main {
     public static final int COUNT_OF_LOOPS = 100;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        PrintStream fileOut = new PrintStream("./log.txt");
+        System.setOut(fileOut);
+
         new Thread(() -> {
             int i = 0;
             while (i != COUNT_OF_LOOPS) {
-                i++;
+                System.out.println(Thread.currentThread().getName() + " value = " + ++i);
             }
-            System.out.println("implements Thread won!");
-            System.exit(0);
         }).start();
 
         new ThreadClass().start();
--- src/main/java/ThreadClass.java
+++ src/main/java/ThreadClass.java
@@ -2,9 +2,7 @@ public class ThreadClass extends Thread {
     public void run() {
         int i = 0;
         while (i != Main.COUNT_OF_LOOPS) {
-            i++;
+            System.out.println(Thread.currentThread().getName() + " value = " + ++i);
         }
-        System.out.println("extends Thread won!");
-        System.exit(0);
     }
 }
